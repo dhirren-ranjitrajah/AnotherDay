2740b469ad902d64706c39bce44834233bf1b260
dranjitrajah <d.ranjitrajah@gmail.com>
2026-08-13 16:27:29 +0800

    design: make nav bar prettier

--- src/renderer/components/Navbar.tsx
+++ src/renderer/components/Navbar.tsx
@@ -10,8 +10,8 @@ export default function Navbar() {
     "min-w-8 min-h-8 w-8 h-8 transition-colors group-hover/navlink:text-primary-hover group-hover/navlink:duration-200";
 
   return (
-    <div className="group top-0 left-0 bottom-0 z-10 w-16 hover:w-32 transition-all duration-400">
-      <nav className="flex flex-col gap-4 bg-background-lowered p-4 duration-400">
+    <div className="group top-0 left-0 bottom-0 h-full py-4 z-10 w-16 hover:w-32 transition-all duration-400 bg-background-lowered/50">
+      <nav className="flex flex-col h-full gap-4 border-r-4 border-background-lowered p-4 duration-400">
         <NavLink
           to="/"
           className={({ isActive }) =>
