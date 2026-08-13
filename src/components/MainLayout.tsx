ae6b6db2538ce3d8f1d75a600da027680d65e92e
dranjitrajah <d.ranjitrajah@gmail.com>
2026-08-13 09:18:59 +0800

    feat: side navbar instead of top

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,13 +1,13 @@
-import { Outlet } from 'react-router-dom'
-import Navbar from './Navbar'
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
 
 export default function MainLayout() {
   return (
-    <div className="flex h-screen flex-col bg-background">
+    <div className="flex min-h-screen min-w-screen flex-row bg-background transition-none">
       <Navbar />
       <main className="flex-1 min-h-0 overflow-y-auto">
         <Outlet />
       </main>
     </div>
-  )
+  );
 }
