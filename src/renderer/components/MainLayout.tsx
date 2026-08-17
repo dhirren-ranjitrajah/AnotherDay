110d39226be8581d8c7f47e1b95f8eef40b99921
dranjitrajah <d.ranjitrajah@gmail.com>
2026-08-17 22:27:30 +0800

    lint: remove unused import

--- src/renderer/components/MainLayout.tsx
+++ src/renderer/components/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import Navbar from "./Navbar";
 import AddTaskModal from "./AddTaskModal";
 import useTaskModal from "../hooks/useTaskModal";
