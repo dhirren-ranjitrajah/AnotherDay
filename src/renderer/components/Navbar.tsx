8aaeea816f2b3d927982d1148b96c51cd972366b
dranjitrajah <d.ranjitrajah@gmail.com>
2026-08-20 07:08:43 +0800

    feat: notification symbol for priorities

--- src/renderer/components/Navbar.tsx
+++ src/renderer/components/Navbar.tsx
@@ -3,13 +3,18 @@ import HomeIcon from "../../assets/icons/home.svg?react";
 import BacklogIcon from "../../assets/icons/list.svg?react";
 import PrioritisationIcon from "../../assets/icons/crosshair.svg?react";
 import CompletedIcon from "../../assets/icons/check.svg?react";
+import useTasks from "../hooks/useTasks";
 
 export default function Navbar() {
+  const { tasks } = useTasks();
+
   const navLinkClass = "group/navlink flex flex-row gap-2 items-center";
   const navLinkTextClass =
     "invisible opacity-0 group-hover:visible group-hover:opacity-100 group-hover:delay-100 group-hover:duration-400 group-hover/navlink:delay-0 group-hover/navlink:duration-200 group-hover/navlink:text-primary-hover";
   const iconClass =
     "min-w-8 min-h-8 w-8 h-8 transition-colors group-hover/navlink:text-primary-hover group-hover/navlink:duration-200";
+  const notificationClass =
+    "absolute top-0 right-0 w-3 h-4 leading-4 bg-error/90 rounded-sm text-center text-sm font-bold";
 
   return (
     <div className="group top-0 left-0 bottom-0 h-full py-4 z-10 w-16 hover:w-40 transition-all duration-400 bg-background-lowered/50">
@@ -42,7 +47,13 @@ export default function Navbar() {
             navLinkClass + (isActive ? " text-primary" : "")
           }
         >
-          <PrioritisationIcon className={iconClass} />
+          <div className="relative">
+            <PrioritisationIcon className={iconClass + " relative"} />
+            {tasks.filter((t) => !t.isCompleted && !t.priority).length > 0 && (
+              <p className={notificationClass}>!</p>
+            )}
+          </div>
+
           <p className={navLinkTextClass}>Prioritise</p>
         </NavLink>
         <NavLink
